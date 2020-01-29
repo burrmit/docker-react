e9183f830623c676782a2b8ed234885c6813df92
Mitchell Burr <ak75@clarica.com>
2020-01-29 13:28:20 -0500

    change the app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Oh what a night!
+          What a cool feature branch!
         </p>
         <a
           className="App-link"
